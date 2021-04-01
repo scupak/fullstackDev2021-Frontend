8da06dd4d2d7b950c16a82b7d3c7d330d8cee4e9
scupak <anto1095@easv365.dk>
2021-04-01 07:10:48 +0200

    stockDTO

--- src/app/stock/shared/stock.service.ts
+++ src/app/stock/shared/stock.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {StockSocket} from '../../app.module';
 import {Stock} from './stock.model';
 import {Observable} from 'rxjs';
+import {StockDTO} from './stockDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +11,21 @@ export class StockService {
 
   constructor(private stockSocket: StockSocket ) { }
 
-  UpdateStock(stock: Stock): void{
+  UpdateStock(stock: StockDTO): void{
    this.stockSocket.emit('update', stock);
    console.log('Update emitted!!!');
   }
 
-  listenForUpdate(): Observable<Stock>{
-    const stock = this.stockSocket.fromEvent<Stock>('stockUpdate');
+  listenForUpdate(): Observable<StockDTO>{
+    const stock = this.stockSocket.fromEvent<StockDTO>('stockUpdate');
     console.log(stock);
     return stock;
 
   }
 
-  ListenForAllStocks(): Observable<Stock[]>{
+  ListenForAllStocks(): Observable<StockDTO[]>{
 
-        return this.stockSocket.fromEvent<Stock[]>('stocks');
+        return this.stockSocket.fromEvent<StockDTO[]>('stocks');
 
   }
   listenForError(): Observable<string> {
